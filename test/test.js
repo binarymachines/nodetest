9969b56018420caee3523bb3670cce8c78133c6d
Dexter Taylor <binarymachineshop@gmail.com>
2016-08-11 07:56:11 -0700

    ...

--- test/test.js
+++ test/test.js
@@ -1,9 +1,12 @@
-// simple test using Mocha
-const snapclient = require('../snapclient')
-
+// exploring tests using Mocha 
+// and the Chai assertions library.
+// -DT
 
 var assert = require('assert');
 var expect = require('chai').expect;
+const snapclient = require('../snapclient')
+
+
 
 describe('the sum of', function() {
   describe('2 and 2', function() {
@@ -14,10 +17,13 @@ describe('the sum of', function() {
 });
 
 
+// the reference to 'done()' is part of the async capability
+// in Mocha, but I'm not actually using it here
+
 describe('when we call the Snap microservice', function(){
-    describe('the endpoint /user/<id>', function() {
-	var jsondoc = snapclient.talkToSnap();	
-	it('should return something', function(done) {	    
+    describe('the endpoint /user/<id>', function() {	
+	it('should return something', function(done) {
+	    var jsondoc = snapclient.talkToSnap();	
 	    expect(jsondoc).to.exist;
 	    done();
 	});
